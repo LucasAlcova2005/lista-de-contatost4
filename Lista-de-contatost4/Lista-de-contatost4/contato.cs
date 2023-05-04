48f93e21acc0fc1d64ee83115db39f34afd351ce
LucasAlcova2005 <lucasdocarmoalcova2005@gmail.com>
2023-05-04 15:25:58 -0300

    Contato
    Lista de contato

---
+++ Lista-de-contatost4/Lista-de-contatost4/contato.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_contatost4
+{
+    internal class contato
+    {
+        //variaveis internas da classe contato.
+        //a palavra-chave "private" indica que somente a classe
+        //*contato* tem acesso.
+
+        private string primeiroNome;
+        private string sobrenome;
+        private string telefone; 
+
+        // PROPRIEDADES (GET e SET)
+        public string PrimeiroNome
+        {
+            get { return primeiroNome; }
+            set { primeiroNome = value; }
+        }
+
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set
+            {
+                if (value.Length == 11)
+                    Telefone = value;
+                else
+                    Telefone = "00-00000-0000";
+            }
+        }
+
+        // Metodo construtor da classe
+        public contato()
+        {
+            PrimeiroNome = "José";
+            Sobrenome = "Da Silva";
+            Telefone = "11-99782-8370"
+        }
+        //sobrecarga do metodo construtor da classe
+        public contato(string primeiroNome, string sobrenome, string telefone)
+        {
+            PrimeiroNome = primeiroNome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+        public override string ToString()
+        {
+            string saida = String.Empty;
+        }
+
+    }
+}
